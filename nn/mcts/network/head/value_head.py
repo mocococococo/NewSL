da18c5c6ad40eb6db695d719f8c57e30b19803f8
mococo <115209810+mocococococo@users.noreply.github.com>
2025-12-09 06:50:01 +0900

    Refactor ValueHead to use BOARD_SIZE constants

--- nn/mcts/network/head/value_head.py
+++ nn/mcts/network/head/value_head.py
@@ -3,15 +3,16 @@
 import torch
 from torch import nn
 
+from board.constant import BOARD_SIZE_X, BOARD_SIZE_Y
+
 
 class ValueHead(nn.Module):
     """Value headの実装クラス。
     """
-    def __init__(self, board_size: int, channels: int, momentum: float=0.01):
+    def __init__(self, channels: int, momentum: float=0.01):
         """Value headの初期化処理。
 
         Args:
-            board_size (int): 碁盤のサイズ。
             channels (int): 共通ブロック部の畳み込み層のチャネル数。
             momentum (float, optional): バッチ正則化層のモーメンタムパラメータ. Defaults to 0.01.
         """
@@ -20,7 +21,7 @@ class ValueHead(nn.Module):
         self.conv_layer = nn.Conv2d(in_channels=channels, out_channels=1, \
             kernel_size=3, padding=1, bias=False)
         self.bn_layer = nn.BatchNorm2d(num_features=1, eps=2e-5, momentum=momentum)
-        self.fc_layer1 = nn.Linear(board_size ** 2, 256)
+        self.fc_layer1 = nn.Linear(BOARD_SIZE_X * BOARD_SIZE_Y, 256)
         self.fc_layer2 = nn.Linear(256, 17)
         self.relu = nn.ReLU()
 
@@ -36,7 +37,7 @@ class ValueHead(nn.Module):
         hidden = self.relu(self.bn_layer(self.conv_layer(input_plane)))
         batch_size, _, height, width = hidden.shape
         reshape = hidden.reshape(batch_size, height * width)
-        fc1 = self.fc_layer1(reshape)
+        fc1 = self.relu(self.fc_layer1(reshape))
         value_out = self.fc_layer2(fc1)
 
         return value_out
