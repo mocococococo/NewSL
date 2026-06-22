28e5ca717122ceb2195c86910d9873c8d6d4f520
mocococococo <mococ.cococo.03.11@gmail.com>
2026-06-22 15:37:07 +0900

    モデル追加

--- transformer/shot_generator.py
+++ transformer/shot_generator.py
@@ -523,14 +523,14 @@ def main(chunk_start: int, chunk_end: int) -> None:
         target_end=9,
         use_end_augmentation=False,
         use_score_diff_augmentation=False,
-        target_shot=[13],
+        target_shot=[12],
         model=Path(__file__).resolve().parents[1] / "model" / "js20000CP-32-9-LeaRate1000-vx32-vy25-batchsize1024.bin",
         use_transformer=True,
         transformer_model=Path(__file__).resolve().parents[1]
         / "model"
-        / "transformer-sl-9-14-model-06-22-adamw-epoch50-shot-non-end-extended.bin",
+        / "transformer-sl-9-13-model-06-22-adamw-epoch50-shot-non-end-extended.bin",
         transformer_target_end=[9],
-        transformer_target_shot=[14],
+        transformer_target_shot=[13],
         max_simulations=1022,
         use_gpu=False,
         shuffle_seed=12345,
