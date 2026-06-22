1eba16b3b3c4fb21302587089460473c0e9fc098
mocococococo <mococ.cococo.03.11@gmail.com>
2026-06-22 10:41:00 +0900

    モデル追加

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ def train_main(model_name: str, use_gpu: bool):
     print(f"finish learning model {model_name} !!")
 
 @click.command()
-@click.option('--model-name', type=click.STRING, default="transformer-sl-9-6-model-06-19-adamw-epoch50-shot", help="保存するモデルの名前の指定")
+@click.option('--model-name', type=click.STRING, default="transformer-sl-9-15-model-06-22-adamw-epoch50-shot-non-end-extended", help="保存するモデルの名前の指定")
 @click.option('--use-gpu', type=click.BOOL, default=True, help="GPUの使用")
 def train_transformer_main(model_name: str, use_gpu: bool):
     # プログラムのディレクトリ
